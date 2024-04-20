9550f9482616822953bfad6f20de3fe4b4cbc4a9
kubaejj <jakub.jezbera.023@pslib.cz>
2024-04-20 11:52:24 +0200

    sss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-  
-    },
-    screens: {
-      sm:'480px',
-      lg:'97px',
-    },
-    spacing: {
-      '1': '100px', // class="mb-1, to znamena margin 8px"
-      '2': '12px',
-      '3': '16px',
-      '4': '24px',
-      '5': '32px',
-      '6': '48px',
-    }
-  },
-  plugins: [],
+
+  content: [
+      "./node_modules/flowbite/**/*.js"
+  ]
+
 }
